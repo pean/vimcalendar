a367492f840ae99b71452813dcdb0be7533ddaf8
Peter Andersson <pean@wa.se>
2019-11-21 09:08:54 +0100

    Build actions copies files to public dir

--- .github/actions/build/index.js
+++ .github/actions/build/index.js
@@ -1,1 +1,20 @@
 console.log("Build action runs")
+
+var fs = require('fs');
+
+var src = "days";
+var dest = "public";
+
+fs.readdir(src, function(err, files) {
+  if (err) {
+    return console.log("Unable to find directory" + dir)
+  }
+  files.forEach(function(file) {
+    srcFile = src + "/" + file
+    destFile = dest + "/" + file
+    fs.copyFile(srcFile, destFile, (err) => {
+      if(err) throw err;
+      console.log(srcFile + " was copied to " + destFile)
+    });
+  });
+});
